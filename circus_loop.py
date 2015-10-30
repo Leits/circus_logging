ee30dace98ff4349db3f397c29aaf99ab700142f
Leits <leits@quintagroup.org>
2015-10-30 18:17:07 +0200

    Init

---
+++ circus_loop.py
@@ -0,0 +1,14 @@
+from time import sleep
+import logging
+
+logger = logging.getLogger()
+
+
+#logger.info('LOOP1')
+
+#print "SDS"
+
+while True:
+    print "LOOP"
+    logger.info('LOOP333')
+    sleep(1)
